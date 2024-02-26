e549e0f50bb904a0e1b33f20fe9cf1f7ce186e64
Utrope <simevain@gmail.com>
2024-02-26 22:32:51 +0100

    website

---
+++ audiotool.js
@@ -0,0 +1,50 @@
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+const tracks = [];
+const trackSources = ['./vocals.wav', './accompaniment.wav'];
+
+trackSources.forEach((trackPath, index) =>
+{
+	fetch(trackPath)
+	.then(response => response.arrayBuffer())
+	.then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
+	.then(audioBuffer => 
+	{
+		const trackSource = audioContext.createBufferSource();
+		trackSource.buffer = audioBuffer;
+
+		const gainNode = audioContext.createGain();
+		trackSource.connect(gainNode);
+		gainNode.connect(audioContext.destination);
+		tracks.push({source: trackSource, gainNode: gainNode, isMuted:false});
+	});
+});
+
+function toggleMute(trackIndex) 
+{
+	const track = tracks[trackIndex];
+	if (track.isMuted)
+	{
+		track.gainNode.gain.value = 1;
+		track.isMuted = false;
+	}
+	
+	else
+	{
+		track.gainNode.gain.value = 0;
+		track.isMuted = true;
+	}
+}
+
+function playAllTracks()
+{
+	const startTime = audioContext.currentTime  + 0.1;
+	tracks.forEach(track => 
+	{
+		if (!track.source.startCalled) 
+		{
+			track.source.start(startTime);
+			track.source.startCalled = true;
+		}
+	});
+}
